ce64d324533215e0f4f011bbcab34d25de6bc537
Ali Abbas <alioabbas10@gmail.com>
2023-05-14 11:33:10 -0400

    added tabs

--- client/src/pages/otdPage.jsx
+++ client/src/pages/otdPage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect} from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { HistoryContext } from '../contexts/HistoryContext';
 import { useParams } from 'react-router-dom';
 
@@ -6,6 +6,8 @@ const OtdPage = () => {
   console.log("otd rendered");
   const { data, error, fetchData } = useContext(HistoryContext);
   const { month, day } = useParams();
+
+  const [activeTab, setActiveTab] = useState("Events");
 
   useEffect(() => {
     if (month && day) {
@@ -20,6 +22,10 @@ const OtdPage = () => {
     }
   }, [month, day]);
 
+  const handleClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   if (error) {
     return <div>Error fetching data: {error.message}</div>;
   }
@@ -27,24 +33,50 @@ const OtdPage = () => {
   if (!data || !data.summaries) {
     return <div>Loading data...</div>;
   }
-  
+
   const renderItems = (title, summary) => (
     <>
-      <h2>{title}</h2>
       <p>{summary}</p>
     </>
   );
 
   return (
     <div>
-    <h1>{data.title}</h1>
-    {renderItems("Events", data.summaries.events)}
-    {renderItems("Births", data.summaries.births)}
-    {renderItems("Deaths", data.summaries.deaths)}
-  </div>
+      <div className="tab-container"> 
+        <div className='tabs'>
+          <div className='tab-header'>
+            <div className={activeTab === 'Events' ? 'active' : ''} onClick={() => handleClick("Events")}>
+              <p>Events</p>
+            </div>
+            <div className={activeTab === 'Births' ? 'active' : ''} onClick={() => handleClick("Births")}>
+              <p>Births</p>
+            </div>
+            <div className={activeTab === 'Deaths' ? 'active' : ''} onClick={() => handleClick("Deaths")}>
+              <p>Deaths</p>
+            </div>
+            <div className={activeTab === 'History' ? 'active' : ''} onClick={() => handleClick("History")}>
+              <p>History</p>
+            </div>
+          </div>
+          <div className='tab-body'>
+            {activeTab === 'Events' && renderItems("Events", data.summaries.events)}
+            {activeTab === 'Births' && renderItems("Births", data.summaries.births)}
+            {activeTab === 'Deaths' && renderItems("Deaths", data.summaries.deaths)}
+            {activeTab === 'History'}
+          </div>
+        </div>
+        <div>
+          <img src={`${process.env.PUBLIC_URL}/pillar.png`}
+            alt="Pillar-right"
+            className="right-pillar"/>
+          {/* <h1 className='otd-date'>{data.title}</h1> */}
+          <img src={`${process.env.PUBLIC_URL}/pillar.png`}
+          alt="Pillar-left"
+          className="left-pillar"/>
+        </div>
+      </div>
+    </div>
   );
 };
 
-
 export default OtdPage;
-
